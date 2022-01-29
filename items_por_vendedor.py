cb8b792bb5611c8df568596288f18d9c7a7b3274
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-01-29 13:14:44 -0300

    Se modularizo un poco el codigo

--- items_por_vendedor.py
+++ items_por_vendedor.py
@@ -2,60 +2,58 @@ import requests
 import json
 import sys
 
+# Función llamada a la API
+def call_api(path, token):
+    
+    # Llamada a la API de ML
+    call_items = requests.get(path, headers={'Authorization': token})
+
+    # json.load devuelve objeto de tipo diccionario sobre el que se puede iterar
+    response = json.loads(call_items.content)
+
+    return response
+
+#Función imprimir log
+def write_log(seller_id, item_id, item_name, category_id, category_name):
+
+    log_name = 'log' + seller_id + '.txt'
+    with open(log_name, 'a') as log:
+        print(item_id, ',', item_name, ',', category_id, ',',category_name, file = log)
+        log.close()
+
 # Función que logea
 def logger(seller_id):
     
-    print('Logeando el usuario: ', seller_id)
-
-    # Path API ML que retorna los items de un vendedor para un determinado site_id y un seller_id
-    path_items = 'https://api.mercadolibre.com/sites/MLA/search?seller_id=' + seller_id
-
-    # Llamada a la API de ML
-    call_items = requests.get(path_items, 
-                            headers={'Authorization': ''})
-
-    # Contenido del response
-    content_items = call_items.content
-
-    # json.load devuelve objeto de tipo diccionario sobre el que se puede iterar
-    json_items = json.loads(content_items)
+    json_items = call_api('https://api.mercadolibre.com/sites/MLA/search?seller_id=' + seller_id, token)
 
     # Condición para logear = que existan items.
     if json_items['results']:
-        
+
+        print('Inicio del logeo del usuario: ', seller_id)
+
         # Iteración sobre el diccionario
         for i in json_items['results']:
-            
-            # Path API ML que retorna los items de un vendedor para un determinado site_id y un seller_id
-            path_category = 'https://api.mercadolibre.com/categories/' + i['category_id']
 
-            # Llamada a la API de ML
-            call_category = requests.get(path_category, 
-                                headers={'Authorization': ''})
-
-            # Contenido del response
-            content_category = call_category.content
-
-            # json.load devuelve objeto de tipo diccionario sobre el que se puede iterar
-            json_category = json.loads(content_category)
+            json_category = call_api('https://api.mercadolibre.com/categories/' + i['category_id'], token)
 
             # Write del log
-            log_name = 'log' + seller_id + '.txt'
-            with open(log_name, 'a') as log:
-                print(i['id'], '|', i['title'], '|', i['category_id'], '|',json_category['name'], file = log)
-                log.close()
+            write_log(seller_id, i['id'], i['title'], i['category_id'], json_category['name'])
+        
+        print('Fin del logeo del usuario: ', seller_id)
+
     else:
         # Print en caso de que no haya items
         print('El usuario ', seller_id, ' no posee items o es incorrecto.')
 
-    print('Fin del logeo del usuario: ', seller_id)
 
-    
-    
 if __name__ == '__main__':
-    
+
     # Lista a partir de los parametros ingresados
     parameters = sys.argv
 
-    for i in range(1, len(parameters)):
+    # Token
+    token = 'Bearer ' + parameters[1]
+
+    # Sellers ID
+    for i in range(2, len(parameters)):
         logger(parameters[i])
